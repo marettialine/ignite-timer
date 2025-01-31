9ff7c9d79460bb52cbd9c74754983b6c2a880470
marettialine <alinesmaretti@gmail.com>
2025-01-31 16:23:51 -0300

    fix: apply eslint fixes to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/defaults";
-import { GlobalStyle } from "./styles/global";
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/defaults'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
