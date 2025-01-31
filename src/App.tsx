2a973885b49948b59dfaee75694fecf1475519d8
marettialine <alinesmaretti@gmail.com>
2025-01-31 15:14:29 -0300

    feat: add GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/defaults";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Initial Project Setup</h1>
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
