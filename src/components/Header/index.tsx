b4d02882a55bf3b86c385def2fc9e4db262298bb
marettialine <alinesmaretti@gmail.com>
2025-01-31 17:32:41 -0300

    chore: refactor Header folders and files structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import { HeaderContainer } from './styles'
 
-import logoIgnite from '../assets/logo.svg'
+import logoIgnite from '../../assets/logo.svg'
 import { Scroll, Timer } from 'phosphor-react'
 import { NavLink } from 'react-router-dom'
 
